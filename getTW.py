550f095ae0004235660c234c64987d4138a628f4
Passawee Jantakorn <KlutoNium@1st-Knium.local>
2016-03-02 16:28:21 +0900

    getting title and keywords

---
+++ getTW.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+import urllib.request
+from bs4 import BeautifulSoup
+
+def getTW(url):
+	useragent = 'Mozilla/5.0'
+	req = urllib.request.Request(url)
+	req.add_header("User-agent",useragent)
+	res = urllib.request.urlopen(req)
+	html = res.read().decode()
+	soup = BeautifulSoup(html)
+	
+	title = soup.find("title")
+	section = soup.findAll("div",attrs = {"class":"section"})
+	section = section[4]
+	words = section.findAll("a")
+	title = title.getText().split('\t')
+	title = title[-2].strip('\n')
+	for i in range(len(words)):
+		words[i] = words[i].getText()
+	dic = {"title":title , "keywords":words}
+	print(dic)
+	return dic
+
+def main(url):
+	getTW(url)
+
+if __name__ == '__main__':
+	main(url)
