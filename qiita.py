9c21559a5ffa1f97371555f7df3eabff22adaf4b
Passawee Jantakorn <KlutoNium@1st-Knium.local>
2016-03-04 15:11:52 +0900

    Get IDs that a user added his stock, then get tags in his stocked articles

--- qiita.py
+++ qiita.py
@@ -18,13 +18,13 @@ def getTag(idList): #記事IDをリストで渡すとリスト上全ての記事
     return words
 
 def makeUniqDict(keywords):
-	uniqDict = {}
-	for word in keywords:
-		uniqDict[word] = 0
+    uniqDict = {}
+    for word in keywords:
+        uniqDict[word] = 0
     return uniqDict
 
 def countUniqWords(uniqDict,keywords):
-	for word in keywords:
-		if word in uniqDict:
-			uniqDict[word] += 1
+    for word in keywords:
+        if word in uniqDict:
+            uniqDict[word] += 1
     return uniqDict
