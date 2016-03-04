115338730aaa9bc52c27cbf37f67cf03b254eb95
Passawee Jantakorn <KlutoNium@1st-Knium.local>
2016-03-04 12:48:37 +0900

    Get IDs that a user added his stock, then get tags in his stocked articles

---
+++ qiita.py
@@ -0,0 +1,18 @@
+from qiita_v2.client import QiitaClient
+
+def getFavID(usrid): #ユーザIDを渡すとユーザのお気に入り(ストックした記事)の全ての記事のIDをリストにして返します
+    client = QiitaClient(access_token='63cea801ef0ad9d73cf028fcd34fd59941f11473')
+    idList = []
+    favList = client.list_user_stocks(usrid).to_json()
+    for fav in favList:
+        idList.append(fav['id'])
+    return idList
+
+def getTag(idList): #記事IDをリストで渡すとリスト上全ての記事のタグをキーワードで返します
+    client = QiitaClient(access_token='63cea801ef0ad9d73cf028fcd34fd59941f11473')
+    words = []
+    for articleid in idList:
+        item = client.get_item(articleid).to_json()
+        for i in item['tags']:
+            words.append(i['name'])
+    return words
